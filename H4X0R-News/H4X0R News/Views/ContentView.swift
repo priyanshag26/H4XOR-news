c8f5c877c9a7aae86ff2ae5037a157a27fc50265
Priyansh Agarwal <187436469+priyanshag26@users.noreply.github.com>
2024-12-24 03:15:11 +0530

    Update ContentView.swift

--- H4X0R-News/H4X0R News/Views/ContentView.swift
+++ H4X0R-News/H4X0R News/Views/ContentView.swift
@@ -1,31 +1,43 @@
 
-
 import SwiftUI
 
 struct ContentView: View {
     
     @ObservedObject var networkManager = NetworkManager()
+    @State private var isDarkMode: Bool = false
     
     var body: some View {
         NavigationView {
             List(networkManager.posts) { post in
                 NavigationLink(destination: DetailView(url: post.url)) {
                     HStack {
+                        Image(systemName: "arrow.up.circle.fill")
                         Text(String(post.points))
+                            .padding(.trailing, 15)
                         Text(post.title)
+                            .padding(.trailing, 10)
                     }
                 }
             }
-            .navigationBarTitle("H4X0R NEWS")
-        }
-        .onAppear {
-            self.networkManager.fetchData()
+            .navigationBarItems(leading: HStack {
+                Text("H4X0R News")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundColor(.primary)
+            }, trailing: Button(action: toggleDarkMode) {
+                Image(systemName: isDarkMode ? "sun.max.fill" : "moon.fill")
+                    .font(.body)
+                    .foregroundColor(isDarkMode ? .yellow : .blue)
+            })
+            .navigationBarTitleDisplayMode(.inline)
+            .preferredColorScheme(isDarkMode ? .dark : .light)
+            .onAppear {
+                self.networkManager.fetchData()
+            }
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    private func toggleDarkMode() {
+        isDarkMode.toggle()
     }
 }
